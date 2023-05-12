a4c6601196bf02639198c5ae96a4c723cf5a23b1
unknown <ankitaggarwal829@gmail.com>
2023-05-13 01:11:16 +0530

    added cors bypass for local react application..

--- src/main/java/com/example/develop/Controller/MainController.java
+++ src/main/java/com/example/develop/Controller/MainController.java
@@ -5,12 +5,13 @@ import com.example.develop.service.DevelopDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController("/dev")
 public class MainController {
 
